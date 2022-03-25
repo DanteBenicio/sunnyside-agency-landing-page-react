20451d146c9653ad35fef962aff32f3410bc5596
DanteBenicio <dantecosta79@gmail.com>
2022-03-25 17:02:05 -0300

    created content file

---
+++ src/components/FirstSection/content.ts
@@ -0,0 +1,27 @@
+/* eslint-disable import/prefer-default-export */
+
+import { ContentRow } from '../../types/content-row';
+
+export const firstContentRow: ContentRow = {
+  title: 'Transform your brand',
+  description: `
+    We are a full-service creative agency specializing in helping brands grow fast.
+    Engage your clients through compelling visuals that do most of the marketing for you.
+  `,
+  desktopImgSrc: 'assets/desktop/image-egg.jpg',
+  mobileImgSrc: 'assets/mobile/image-egg.jpg',
+  altSrc: 'yellow background with an egg',
+  learnMoreColor: 'yellow',
+};
+
+export const secondContentRow: ContentRow = {
+  title: 'Stand out to the right audience',
+  description: `
+    Using a collaborative formula of designers, researchers, photographers, videographers,
+    and copywriters, we'll build and extend your brand in digital places.
+  `,
+  desktopImgSrc: 'assets/desktop/image-cup.jpg',
+  mobileImgSrc: 'assets/mobile/image-cup.jpg',
+  altSrc: 'pink background with a cup',
+  learnMoreColor: 'soft-red',
+};
