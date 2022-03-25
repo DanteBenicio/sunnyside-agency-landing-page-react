6e59e5db5c7ae86d70b0f9f5b9dd56cc44a7fb19
DanteBenicio <dantecosta79@gmail.com>
2022-03-25 17:10:41 -0300

    component SectionHero

--- src/components/SectionHero/index.tsx
+++ src/components/SectionHero/index.tsx
@@ -2,6 +2,11 @@ import './styles.scss';
 
 export default function SectionHero() {
   return (
-    <div>SectionHero</div>
+    <section className="section_container">
+      <div className="section_wrapper">
+        <h1 id="title">We are creatives</h1>
+        <img src="assets/icon-arrow-down.svg" alt="arrow down" aria-hidden="true" width="32" height="92" />
+      </div>
+    </section>
   );
 }
