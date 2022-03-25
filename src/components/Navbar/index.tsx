6b9aa4090854b99575c88c16ef277628224bee7d
DanteBenicio <dantecosta79@gmail.com>
2022-03-25 17:09:57 -0300

    component Navbar

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,25 @@
+import './styles.scss';
+
+export default function Navbar() {
+  return (
+    <header className="navbar_container">
+      <nav className="navbar_wrapper">
+        <img src="assets/logo.svg" alt="logo writed sunnyside" aria-hidden="true" id="logo" width="124" height="24" />
+        <div className="list_button_wrapper">
+          <ul className="list">
+            <li>
+              <a href="#0">About</a>
+            </li>
+            <li>
+              <a href="#1">Services</a>
+            </li>
+            <li>
+              <a href="#2">Projects</a>
+            </li>
+          </ul>
+          <button type="button" className="contact_btn">Contact</button>
+        </div>
+      </nav>
+    </header>
+  );
+}
