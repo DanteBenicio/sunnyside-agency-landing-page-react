f243e47e4479efccd52f0f445d77cec899ea74b4
DanteBenicio <dantecosta79@gmail.com>
2022-03-27 17:17:08 -0300

    burger state

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import './App.css';
 
 function App() {
+  const [burger, setBurger] = useState(false);
+
+  const toggleBurgerValue = () => setBurger((prevState) => !prevState);
+
+  window.addEventListener('resize', (e) => {
+    if (e.target?.innerWidth >= 768) {
+      setBurger(false);
+    }
+  });
+
   return (
     <h1>Hello World</h1>
   );
