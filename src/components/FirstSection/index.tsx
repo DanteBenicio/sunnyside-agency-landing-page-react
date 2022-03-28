ae498cf6e9f72a0e4fced27a34149b514ab44fb5
DanteBenicio <dantecosta79@gmail.com>
2022-03-28 11:45:58 -0300

    render ImagesRow component

--- src/components/FirstSection/index.tsx
+++ src/components/FirstSection/index.tsx
@@ -2,12 +2,14 @@
 import ContentRow from './ContentRow';
 import './styles.scss';
 import { firstContentRow, secondContentRow } from './content';
+import ImagesRow from './ImagesRow';
 
 export default function FirstSection() {
   return (
     <main>
       <ContentRow {...firstContentRow} />
       <ContentRow {...secondContentRow} rowReverse />
+      <ImagesRow />
     </main>
   );
 }
